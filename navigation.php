bb10ac57eb4153e1e3bc537bf47fed1462d88712
jhersy143 <jhersyafernandez@gmail.com>
2017-09-07 04:46:39 -0700

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-toggleable-md  fixed-top" >
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <ul class="nav nav-tabs" role="tablist">
+  <a class="navbar-brand" href="#">A&A ONE STOP SHOP MEDICAL SUPPLY MANAGEMENT SYSTEM</a>
+  <div class="collapse navbar-collapse" id="navbarNav" >
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="linkcolor nav-link"  href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link linkcolor"  href="productlist.php">Product</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="Order.php">Orders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link linkcolor" href="customerlogin.php">Log Out</a>
+      </li>
+    </ul>
+  </div>
+</nav>
